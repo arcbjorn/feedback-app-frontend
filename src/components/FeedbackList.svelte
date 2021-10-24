93145ae55d91380c5314dd50b6ba845a24a87467
arcbjorn <arky.happ@gmail.com>
2021-10-24 22:55:54 +0300

    feat (Components): add FeedbackList

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import FeedBackItem from "./FeedBackItem.svelte";
+import { fade, scale } from "svelte/transition";
+import { FeedbackStore } from '../stores';
+</script>
+
+{#each $FeedbackStore as item (item.id)}
+    <div in:scale out:fade>
+        <FeedBackItem {item} />
+    </div>
+{/each}
